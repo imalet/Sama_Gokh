b325dfd52ee35852cdd845de31b0e7e4a47072db
Jeanne Diop <diopjeannedarc@gmail.com>
2023-12-07 15:14:39 +0000

    modifier admincommune

--- app/Http/Controllers/Api/AdmincommuneController.php
+++ app/Http/Controllers/Api/AdmincommuneController.php
@@ -6,7 +6,9 @@ use Exception;
 use App\Models\User;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use App\Http\Requests\EditAdmincommuneRequest;
 use App\Http\Requests\CreateAdmincommuneRequest;
+
 
 class AdmincommuneController extends Controller
 {
@@ -37,4 +39,33 @@ return response()->json([
     return response()->json($e);
   }
  }  
-}
+
+ public function update(EditAdmincommuneRequest $request, $id)
+ {
+  try{
+$admincommune = User::find($id);
+$admincommune->nom =$request->nom;
+$admincommune->prenom=$request->prenom;
+$admincommune->age=$request->age;
+$admincommune->email=$request->email;
+$admincommune->password=$request->password;
+$admincommune->telephone=$request->telephone;
+$admincommune->etat=$request->etat;
+$admincommune->username=$request->username;
+$admincommune->CNI=$request->CNI;
+$admincommune->sexe=$request->sexe;
+$admincommune->role_id=$request->role_id;
+$admincommune->save();
+
+return response()->json([
+  'status_code' => 200,
+  'status_message' =>'admincommune a été modifié',
+  'data'=>$admincommune
+  ]);
+    }catch(Exception $e){
+      return response()->json($e);
+    }
+   }  
+
+ }
+
