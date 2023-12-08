d959b45caadf0ca2b5f52edeeba9281c5b7239c9
iMALET Benji <imaletbenji58@gmail.com>
2023-12-08 22:45:41 +0000

    HUM

--- app/Http/Controllers/Api/AdmincommuneController.php
+++ app/Http/Controllers/Api/AdmincommuneController.php
@@ -15,93 +15,85 @@ class AdmincommuneController extends Controller
 {
   public function index()
   {
-    try{
+    try {
       return response()->json([
         'status_code' => 200,
-        'status_message' =>'admincommune a été recupéré',
-        'data'=> User::all(),
-        ]);
-    }
-  
-  Catch(Exception $e){
-    return response()->json($e);
-  }
-  
-  
-}
- public function store(CreateAdmincommuneRequest $request)
-  {
-    try{
-$admincommune= new User();
-$admincommune->nom=$request->nom;
-$admincommune->prenom=$request->prenom;
-$admincommune->email=$request->email;
-$admincommune->password=$request->password;
-$admincommune->telephone=$request->telephone;
-$admincommune->etat=$request->etat;
-$admincommune->username=$request->username;
-$admincommune->CNI=$request->CNI;
-$admincommune->sexe=$request->sexe;
-$admincommune->role_id=$request->role_id;
-$admincommune->commune_id=$request->commune_id;
-$admincommune->save();
-
-return response()->json([
-'status_code' => 200,
-'status_message' =>'admincommune a été ajouté',
-'data'=>$admincommune
-]);
-  }catch(Exception $e){
-    return response()->json($e);
-  }
- }  
-
- public function update(EditAdmincommuneRequest $request, $id)
- {
-  try{
-$admincommune = User::find($id);
-$admincommune->nom =$request->nom;
-$admincommune->prenom=$request->prenom;
-$admincommune->email=$request->email;
-$admincommune->password=$request->password;
-$admincommune->telephone=$request->telephone;
-$admincommune->etat=$request->etat;
-$admincommune->username=$request->username;
-$admincommune->CNI=$request->CNI;
-$admincommune->sexe=$request->sexe;
-$admincommune->role_id=$request->role_id;
-$admincommune->commune_id=$request->commune_id;
-$admincommune->save();
-
-return response()->json([
-  'status_code' => 200,
-  'status_message' =>'admincommune a été modifié',
-  'data'=>$admincommune
-  ]);
-    }catch(Exception $e){
+        'status_message' => 'admincommune a été recupéré',
+        'data' => User::all(),
+      ]);
+    } catch (Exception $e) {
       return response()->json($e);
     }
-   }  
+  }
+  public function store(CreateAdmincommuneRequest $request)
+  {
+    try {
+      $admincommune = new User();
+      $admincommune->nom = $request->nom;
+      $admincommune->prenom = $request->prenom;
+      $admincommune->email = $request->email;
+      $admincommune->password = $request->password;
+      $admincommune->telephone = $request->telephone;
+      $admincommune->etat = $request->etat;
+      $admincommune->username = $request->username;
+      $admincommune->CNI = $request->CNI;
+      $admincommune->sexe = $request->sexe;
+      $admincommune->role_id = $request->role_id;
+      $admincommune->commune_id = $request->commune_id;
+      $admincommune->save();
 
-   public function archiver($id)
-   {
-    
-    try{
-    $admincommune = User::find($id);
-    $admincommune->etat='inactif';
-    $admincommune->save();
-    
-    return response()->json([
-      'status_code' => 200,
-      'status_message' =>'admincommune a été archivé',
-      'data'=>$admincommune
+      return response()->json([
+        'status_code' => 200,
+        'status_message' => 'admincommune a été ajouté',
+        'data' => $admincommune
       ]);
-        }catch(Exception $e){
-          return response()->json($e);
-        }
-       } 
+    } catch (Exception $e) {
+      return response()->json($e);
+    }
+  }
 
-   
+  public function update(EditAdmincommuneRequest $request, $id)
+  {
+    try {
+      $admincommune = User::find($id);
+      $admincommune->nom = $request->nom;
+      $admincommune->prenom = $request->prenom;
+      $admincommune->email = $request->email;
+      $admincommune->password = $request->password;
+      $admincommune->telephone = $request->telephone;
+      $admincommune->etat = $request->etat;
+      $admincommune->username = $request->username;
+      $admincommune->CNI = $request->CNI;
+      $admincommune->sexe = $request->sexe;
+      $admincommune->role_id = $request->role_id;
+      $admincommune->commune_id = $request->commune_id;
+      $admincommune->save();
 
- }
+      return response()->json([
+        'status_code' => 200,
+        'status_message' => 'admincommune a été modifié',
+        'data' => $admincommune
+      ]);
+    } catch (Exception $e) {
+      return response()->json($e);
+    }
+  }
 
+  public function archiver($id)
+  {
+
+    try {
+      $admincommune = User::find($id);
+      $admincommune->etat = 'inactif';
+      $admincommune->save();
+
+      return response()->json([
+        'status_code' => 200,
+        'status_message' => 'admincommune a été archivé',
+        'data' => $admincommune
+      ]);
+    } catch (Exception $e) {
+      return response()->json($e);
+    }
+  }
+}
--- app/Http/Controllers/Api/MaireController.php
+++ app/Http/Controllers/Api/MaireController.php
@@ -1,6 +1,7 @@
 <?php
 
 namespace App\Http\Controllers\Api;
+
 use Exception;
 use App\Models\User;
 use Illuminate\Http\Request;
@@ -10,91 +11,86 @@ use App\Http\Requests\CreateMaireRequest;
 
 class MaireController extends Controller
 {
-    public function index()
-    {
-      try{
-        return response()->json([
-          'status_code' => 200,
-          'status_message' =>'maire a été recupéré',
-          'data'=> User::all(),
-          ]);
-      }
-    
-    Catch(Exception $e){
-      return response()->json($e);
-    }
-    
-    
-  }
-    public function store(CreateMaireRequest $request)
+  public function index()
   {
-    try{
-$maire= new User();
-$maire->nom =$request->nom;
-$maire->prenom=$request->prenom;
-$maire->email=$request->email;
-$maire->password=$request->password;
-$maire->telephone=$request->telephone;
-$maire->etat=$request->etat;
-$maire->username=$request->username;
-$maire->CNI=$request->CNI;
-$maire->sexe=$request->sexe;
-$maire->role_id=$request->role_id;
-$maire->commune_id=$request->commune_id;
-$maire->save();
-
-return response()->json([
-'status_code' => 200,
-'status_message' =>'Maire a été ajouté',
-'data'=>$maire
-]);
-  }catch(Exception $e){
-    return response()->json($e);
-  }
- } 
- public function update(EditMaireRequest $request, $id)
- {
-  try{
-$maire = User::find($id);
-$maire->nom =$request->nom;
-$maire->prenom=$request->prenom;
-$maire->email=$request->email;
-$maire->password=$request->password;
-$maire->telephone=$request->telephone;
-$maire->etat=$request->etat;
-$maire->username=$request->username;
-$maire->CNI=$request->CNI;
-$maire->sexe=$request->sexe;
-$maire->role_id=$request->role_id;
-$maire->commune_id=$request->commune_id;
-$maire->save();
-
-return response()->json([
-  'status_code' => 200,
-  'status_message' =>'Miare a été modifié',
-  'data'=>$maire
-  ]);
-    }catch(Exception $e){
+    try {
+      return response()->json([
+        'status_code' => 200,
+        'status_message' => 'maire a été recupéré',
+        'data' => User::all(),
+      ]);
+    } catch (Exception $e) {
       return response()->json($e);
     }
-   }  
+  }
+  public function store(CreateMaireRequest $request)
+  {
+    try {
+      $maire = new User();
+      $maire->nom = $request->nom;
+      $maire->prenom = $request->prenom;
+      $maire->email = $request->email;
+      $maire->password = $request->password;
+      $maire->telephone = $request->telephone;
+      $maire->etat = $request->etat;
+      $maire->username = $request->username;
+      $maire->CNI = $request->CNI;
+      $maire->sexe = $request->sexe;
+      $maire->role_id = $request->role_id;
+      $maire->commune_id = $request->commune_id;
+      $maire->save();
 
-   public function archiver($id)
-   {
-    
-    try{
-    $maire = User::find($id);
-    $maire->etat='inactif';
-    $maire->save();
-    
-    return response()->json([
-      'status_code' => 200,
-      'status_message' =>'Maire a été archivé',
-      'data'=>$maire
+      return response()->json([
+        'status_code' => 200,
+        'status_message' => 'Maire a été ajouté',
+        'data' => $maire
       ]);
-        }catch(Exception $e){
-          return response()->json($e);
-        }
-       } 
- 
+    } catch (Exception $e) {
+      return response()->json($e);
+    }
+  }
+  public function update(EditMaireRequest $request, $id)
+  {
+    try {
+      $maire = User::find($id);
+      $maire->nom = $request->nom;
+      $maire->prenom = $request->prenom;
+      $maire->email = $request->email;
+      $maire->password = $request->password;
+      $maire->telephone = $request->telephone;
+      $maire->etat = $request->etat;
+      $maire->username = $request->username;
+      $maire->CNI = $request->CNI;
+      $maire->sexe = $request->sexe;
+      $maire->role_id = $request->role_id;
+      $maire->commune_id = $request->commune_id;
+      $maire->save();
+
+      return response()->json([
+        'status_code' => 200,
+        'status_message' => 'Miare a été modifié',
+        'data' => $maire
+      ]);
+    } catch (Exception $e) {
+      return response()->json($e);
+    }
+  }
+
+  public function archiver($id)
+  {
+
+    try {
+      $maire = User::find($id);
+      $maire->etat = 'inactif';
+      $maire->save();
+
+      return response()->json([
+        'status_code' => 200,
+        'status_message' => 'Maire a été archivé',
+        'data' => $maire
+      ]);
+    } catch (Exception $e) {
+      return response()->json($e);
+    }
+  }
 }
