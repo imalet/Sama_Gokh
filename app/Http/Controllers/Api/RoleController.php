73d0bff05b9ae5acfa3bb6649f7aeaf47a941284
iMALET Benji <imaletbenji58@gmail.com>
2023-12-08 23:52:51 +0000

    HUM DEMAIN

--- app/Http/Controllers/Api/RoleController.php
+++ app/Http/Controllers/Api/RoleController.php
@@ -6,10 +6,16 @@ use App\Models\Role;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\RoleRegisterRequest;
+use App\Http\Resources\RoleResource;
 
 class RoleController extends Controller
 {
-    public function register(RoleRegisterRequest $request){
+
+    public function index(RoleRegisterRequest $request){
+        return response("OK TOP",200);
+    }
+
+    public function store(RoleRegisterRequest $request){
         $role = new Role();
         $role->nom = $request->nom;
         $role->save();
--- routes/api.php
+++ routes/api.php
@@ -30,135 +30,121 @@ use App\Http\Controllers\Api\AdmincommuneController;
 | be assigned to the "api" middleware group. Make something great!
 |
 */
-// Les Routes de création de comptes utilisateurs et de création de roles
+
+// ---------------------------------REGISTER & LOGIN & RESET MOT DE PASSE-------------------------------
+
+// Les Routes de création de comptes utilisateurs
 Route::post('/register', [UserController::class, 'register']);
-Route::post('register/role', [RoleController::class, 'register']);
 //La Routes de connexion  d'un utilisateur
 Route::post('/login', [UserController::class, 'login']);
+//Changer mon mot de passe []
+Route::put('/reset/password', [UserController::class, 'resetPassword']);
 
-// Lien qui permet aux applications clients(Angular, React, Node ...) de se connecter 
 
-// Route::get('link', function(){
-//     return 'Here is the link.';
-// });
-
-// Les Routes de Roles 
-
+//-----------------------------------------ROLES-----------------------------------------------------------------
 // Afficher Liste Role 
 Route::get('roles', [RoleController::class, 'index']);
-
 // Ajouter un role dans la base de données 
-Route::post('/role/store',[RoleController::class,'store'])->name("role.add"); 
-
+Route::post('/role/store', [RoleController::class, 'store'])->name("role.add");
 // Modifier un role 
-Route::put('/role/update/{role}',[RoleController::class,'update'])->name("role.update");
+Route::put('/role/update/{role}', [RoleController::class, 'update'])->name("role.update");
 
-// ----------------------------------------------------------------------------------------------------------
-// ----------------------------------------------------------------------------------------------------------
+// Collection rôle
+Route::get('/ressource/roles', function () {
+    $roles = Role::all();
+    return RoleResource::collection($roles);
+});
 
-// Les Routes de Villes 
+
+// ----------------------------------------VILLE------------------------------------------------------------------
 // Afficher Liste Villes 
 Route::get('villes', [VilleController::class, 'index']);
-
 // Ajouter une ville dans la base de données 
-Route::post('/ville/store',[VilleController::class,'store'])->name("ville.add"); 
-
+Route::post('/ville/store', [VilleController::class, 'store'])->name("ville.add");
 // Modifier une ville 
-Route::put('/ville/update/{ville}',[VilleController::class,'update'])->name("ville.update");
-
-
-// ----------------------------------------------------------------------------------------------------------
-// ----------------------------------------------------------------------------------------------------------
-
-// Les Routes de communes
-// Afficher Liste communes 
-Route::get('communes', [CommuneController::class, 'index']);
-
-// Ajouter une commune dans la base de données 
-Route::post('/commune/store',[CommuneController::class,'store'])->name("commune.add"); 
-
-// Modifier une commune 
-Route::put('/commune/update/{commune}',[CommuneController::class,'update'])->name("commune.update");
-
-// Test Ajouter User
-Route::post('/user/store',[UserController::class,'store']); 
+Route::put('/ville/update/{ville}', [VilleController::class, 'update'])->name("ville.update");
 
 // Collection Ville
-Route::get('/ressource/villes', function(){
+Route::get('/ressource/villes', function () {
     $villes = Ville::all();
     return VilleResource::collection($villes);
 });
 
+// ----------------------------------------COMMMUNE------------------------------------------------------------------
+// Afficher Liste communes 
+Route::get('communes', [CommuneController::class, 'index']);
+// Ajouter une commune dans la base de données 
+Route::post('/commune/store', [CommuneController::class, 'store'])->name("commune.add");
+// Modifier une commune 
+Route::put('/commune/update/{commune}', [CommuneController::class, 'update'])->name("commune.update");
+// Test Ajouter User
+Route::post('/user/store', [UserController::class, 'store']);
+
 // Collection commune
-Route::get('/ressource/communes', function(){
+Route::get('/ressource/communes', function () {
     $communes = Commune::all();
     return CommuneResource::collection($communes);
 });
 
-// Collection rôle
-Route::get('/ressource/roles', function(){
-    $roles = Role::all();
-    return RoleResource::collection($roles);
-});
- 
-//Collection resource Admincommune
-Route::get('projet/resourceadmincommune',function(){
-    $admincommune=User::all();
-    return AdmincommuneResource::collection($admincommune);
-});
-//pour achiver admin_commune
-Route::put('admin_commune/archive/{id}',[AdmincommuneController::class, 'archiver']);
-//pour modifier admin_commune
- Route::put('admin_commune/edit/{id}',[AdmincommuneController::class,'update']);
-//ajouter admin_commune
-Route::post('admin_commune/create',[AdmincommuneController::class,'store']);
-//lister admin_commune
-Route::get('admin_commune/lister',[AdmincommuneController::class, 'index']);
 
+// ----------------------------------------MAIRE------------------------------------------------------------------
+
+//lister maire
+Route::get('maire/lister', [MaireController::class, 'index']);
+//ajouter maire
+Route::post('maire/create', [MaireController::class, 'store']);
+//pour modifier Maire
+Route::put('maire/edit/{id}', [MaireController::class, 'update']);
+//pour achiver maire
+Route::put('maire/archive/{id}', [MaireController::class, 'archiver']);
 
 //Collection resource Maire
-Route::get('projet/resourcemaire',function(){
-    $maire=User::all();
+Route::get('projet/resourcemaire', function () {
+    $maire = User::all();
     return MaireResource::collection($maire);
-    
 });
-//lister maire
-Route::get('maire/lister',[MaireController::class, 'index']);
 
-//ajouter maire
-Route::post('maire/create',[MaireController::class,'store']);
-//pour modifier Maire
-Route::put('maire/edit/{id}',[MaireController::class,'update']);
 
-//pour achiver maire
-Route::put('maire/archive/{id}',[MaireController::class, 'archiver']);
+// ----------------------------------------ADMIN_COMMUNE------------------------------------------------------------------
 
+//pour achiver admin_commune
+Route::put('admin_commune/archive/{id}', [AdmincommuneController::class, 'archiver']);
+//pour modifier admin_commune
+Route::put('admin_commune/edit/{id}', [AdmincommuneController::class, 'update']);
+//ajouter admin_commune
+Route::post('admin_commune/create', [AdmincommuneController::class, 'store']);
+//lister admin_commune
+Route::get('admin_commune/lister', [AdmincommuneController::class, 'index']);
+
+//Collection resource Admincommune
+Route::get('projet/resourceadmincommune', function () {
+    $admincommune = User::all();
+    return AdmincommuneResource::collection($admincommune);
+});
+
+
+// ----------------------------------------####------------------------------------------------------------------
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
- //Changer mon mot de passe []
- Route::put('/reset/password', [UserController::class, 'resetPassword']);
-// Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-//     return $request->user();
-//     //la route de deconnexion
-//     Route::post('/logout', [UserController::class, 'logout']);
-// });
-Route::middleware(['auth:sanctum', 'role:SuperAdmin'])->group(function(){
-   //la route de deconnexion
-     Route::post('/logout', [UserController::class, 'logout']);
+
+
+Route::middleware(['auth:sanctum', 'role:SuperAdmin'])->group(function () {
+    //la route de deconnexion
+    Route::post('/logout', [UserController::class, 'logout']);
 });
 
-Route::middleware(['auth:sanctum', 'role:Citoyen'])->group(function(){
+Route::middleware(['auth:sanctum', 'role:Citoyen'])->group(function () {
 
-  //la gestion du profil d'un citoyen : 
-  //Modifier les informations de profils du  compte du citoyen
-  Route::put('/edit/citizen/{user}', [UserController::class, 'update']);
-  //Archiver leFs informations un compte citoyen
-  Route::put('/archive/citizen/{user}', [UserController::class, 'archive']);
- 
+    //la gestion du profil d'un citoyen : 
 
-  //la route de deconnexion
-Route::post('/logout', [UserController::class, 'logout']);
+    //Modifier les informations de profils du  compte du citoyen
+    Route::put('/edit/citizen/{user}', [UserController::class, 'update']);
 
+    //Archiver leFs informations un compte citoyen
+    Route::put('/archive/citizen/{user}', [UserController::class, 'archive']);
+
+    //la route de deconnexion
+    Route::post('/logout', [UserController::class, 'logout']);
 });
